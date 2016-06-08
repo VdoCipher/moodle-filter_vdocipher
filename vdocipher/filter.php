049fc97703f18563b48839d38d246e59d92642bb
Vibhav Sinha <v@vsinha.me>
2016-06-08 17:41:42 +0530

    use new embed code

--- vdocipher/filter.php
+++ vdocipher/filter.php
@@ -22,8 +22,18 @@ class filter_vdocipher extends moodle_text_filter {
 				return "Video playback can not be authenticated.";
 			}
 			$OTP = json_decode($OTP)->otp;
-			$output = "<span id='vdo$OTP'></span>";
-			$output .= "<script src='https://de122v0opjemw.cloudfront.net/utils/playerInit.php?otp=$OTP&height=$height&width=$width'></script>";
+			$output =  <<<EOF
+<div id="vdo$OTP" style="height:400px;width:640px;max-width:100%;"></div>
+	<script>
+	(function(v,i,d,e,o){v[o]=v[o]||{}; v[o].add = v[o].add || function V(a){ (v[o].d=v[o].d||[]).push(a);};
+	if(!v[o].l) { v[o].l=1*new Date(); a=i.createElement(d), m=i.getElementsByTagName(d)[0];
+	a.async=1; a.src=e; m.parentNode.insertBefore(a,m);}
+	})(window,document,'script','//de122v0opjemw.cloudfront.net/vdo.js','vdo');
+	vdo.add({
+		o: "$OTP",
+	});
+</script>";
+EOF;
 			return $output;
 		}, $text );
 	}
