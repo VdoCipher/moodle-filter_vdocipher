2e6f8b5ed7f946862220a3da58f378aa7f7039a5
Vibhav Sinha <v@vsinha.me>
2017-01-08 00:12:13 +0530

    added GPL headers

--- settings.php
+++ settings.php
@@ -1,4 +1,24 @@
 <?php
+// This file is part of VdoCipher plugin for Moodle ( moodle-filter_vdocipher ) - https://www.vdocipher.com/
+//
+// moodle-filter_vdocipher is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// moodle-filter_vdocipher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with moodle-filter_vdocipher.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * @package   filter_vdocipher
+ * @copyright 2017, VdoCipher Media Solutions <info@vdocipher.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $settings->add(new admin_setting_configtext('filter_vdocipher/csk',
 	get_string('csk', 'filter_vdocipher'),
--- version.php
+++ version.php
@@ -1,18 +1,18 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of VdoCipher plugin for Moodle ( moodle-filter_vdocipher ) - https://www.vdocipher.com/
 //
-// Moodle is free software: you can redistribute it and/or modify
+// moodle-filter_vdocipher is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// moodle-filter_vdocipher is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with moodle-filter_vdocipher.  If not, see <http://www.gnu.org/licenses/>.
  
 /**
  * @package   filter_vdocipher
